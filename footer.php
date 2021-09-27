8481beea665812fb6620ceb88c2aac5573dcdcda
Ruvick <rudikov.web@ya.ru>
2021-09-27 20:39:54 +0300

    Файлы сайта

--- footer.php
+++ footer.php
@@ -1,6 +1,49 @@
+<footer id="footer" class="footer">
+	<div class="footer__container _container">
 
+		<div class="footer__row d-flex">
+
+			<div class="footer__policy">
+				<p>© 2021 Все права защищены</p>
+				<div class="footer__social d-flex">
+					<a href="#" class="footer__social-icon social-icon-01"></a>
+					<a href="#" class="footer__social-icon social-icon-02"></a>
+				</div>
+			</div>
+
+			<div class="footer__menu-block d-flex">
+				<ul class="footer__menu footer__menu_left">
+					<li><a href="#" class="menu__link">Типовые проекты</a></li> 
+					<li><a href="#" class="menu__link">Квадро</a></li>
+					<li><a href="#" class="menu__link">Коттедж 1</a></li>
+					<li><a href="#" class="menu__link">Коттедж 2</a></li>
+					<li><a href="project.html" class="menu__link">Усадьба</a></li> 
+				</ul>
+
+				<ul class="footer__menu footer__menu_right">
+					<li><a href="#" class="menu__link">О проекте</a></li> 
+					<li><a href="#" class="menu__link">Дома</a></li>
+					<li><a href="gallery.html" class="menu__link">Галерея</a></li>
+					<li><a href="team.html" class="menu__link">Наша команда</a></li>
+					<li><a href="#" class="menu__link">Как добраться</a></li> 
+					<li><a href="#" class="menu__link">Контакты</a></li> 
+				</ul>
+			</div>
+
+			<div class="footer__contacts d-flex">
+				<a href="tel:+7(4712)543488" class="footer__phone">+7 (4712) 54-34-88</a>
+				<!-- <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="footer__phone"><? echo $tel = carbon_get_theme_option("as_phone_1"); ?></a> -->
+				<a href="#callback" class="footer__callback _popup-link">Заказать звонок</a>
+				<p class="footer__address">Курск, ул. 50 лет Октября, д. 100</p>
+				<a href="mailto:info@forestsea.ru" class="footer__email">info@forestsea.ru</a>
+			</div>
+
+		</div>
+
+	</div>
+</footer>
 </div>
 
 <?php wp_footer(); ?>
 </body>
-</html>
+</html> 
--- index.php
+++ index.php
@@ -1,31 +1,370 @@
 <?php get_header(); ?>
 
-<?php get_template_part('template-parts/header-section');?>
+<?php get_template_part('template-parts/header-section'); ?>
 
-	<main class="page"> 
+<main class="page">
 
-			<!-- Вывод записей конкретной таксономии по id -->
-				<?
-					$args = array(
-						'posts_per_page' => 4,
-						'post_type' => 'ultra',
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'ultracat',
-								'field' => 'id',
-								'terms' => array(7)
-							)
-						)
-					);
-					$query = new WP_Query($args);
+	<section id="info" class="info">
 
-					foreach( $query->posts as $post ){
-						$query->the_post();
-						get_template_part('template-parts/product-elem');
-					}  
-					wp_reset_postdata();
-				?>
+		<div class="slider-index _swiper">
 
-	</main>
+			<div class="slider-index__slide slider__slide _bgi">
+				<div class="_container">
+					<h1>Строим дома на века!</h1>
+					<p>Строгая система контроля на каждом этапе строительства. <br> Узнайте о наших гарантиях!</p>
+					<a href="#" class="slider-index__btn btn">узнать подробности</a>
+				</div>
+			</div>
 
-<?php get_footer(); ?> 
+			<div class="slider-index__slide slider__slide _bgi">
+				<div class="_container">
+					<h1>Строим дома на века!</h1>
+					<p>Строгая система контроля на каждом этапе строительства. <br> Узнайте о наших гарантиях!</p>
+					<a href="#" class="slider-index__btn btn">узнать подробности</a>
+				</div>
+			</div>
+
+			<div class="slider-index__slide slider__slide _bgi">
+				<div class="_container">
+					<h1>Строим дома на века!</h1>
+					<p>Строгая система контроля на каждом этапе строительства. <br> Узнайте о наших гарантиях!</p>
+					<a href="#" class="slider-index__btn btn">узнать подробности</a>
+				</div>
+			</div>
+
+			<div class="slider-index__slide slider__slide _bgi">
+				<div class="_container">
+					<h1>Строим дома на века!</h1>
+					<p>Строгая система контроля на каждом этапе строительства. <br> Узнайте о наших гарантиях!</p>
+					<a href="#" class="slider-index__btn btn">узнать подробности</a>
+				</div>
+			</div>
+		</div>
+		<div class="swiper-button swiper-button-next"></div>
+		<div class="swiper-button swiper-button-prev"></div>
+
+	</section>
+
+	<section id="dignity" class="dignity">
+		<div class="_container">
+
+			<div class="dignity__row d-flex">
+
+				<div class="dignity__item d-flex">
+					<div class="dignity__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/dignity/01.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/dignity/01.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="dignity__descp">
+						<h6>Закрытый поселок</h6>
+						<p>Закрытая охраняемая территория</p>
+					</div>
+				</div>
+
+				<div class="dignity__item d-flex">
+					<div class="dignity__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/dignity/02.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/dignity/02.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="dignity__descp">
+						<h6>Легко добраться</h6>
+						<p>Всего 2,5 км от г. Курск</p>
+					</div>
+				</div>
+
+				<div class="dignity__item d-flex">
+					<div class="dignity__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/dignity/03.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/dignity/03.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="dignity__descp">
+						<h6>Инфраструктура</h6>
+						<p>Все необходимое для жизни <br> на территории поселка;</p>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+	</section>
+
+	<section id="about-project" class="about-project">
+		<div class="_container">
+			<h2 class="title">О проекте</h2>
+			<p>
+				Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали от всех живут они в буквенных домах на берегу
+				Семантика большого языкового океана. Маленький ручеек Даль журчит по всей стране и обеспечивает ее всеми необходимыми правилами.
+				Эта парадигматическая страна, в которой жаренные члены предложения залетают прямо в рот. Даже всемогущая пунктуация не имеет власти
+				над рыбными текстами, ведущими безорфографичный образ жизни. Однажды одна маленькая строчка рыбного текста по имени Lorem ipsum решила
+				выйти в большой мир грамматики. Великий Оксмокс предупреждал ее о злых запятых, диких знаках вопроса и коварных точках с запятой,
+				но текст не дал сбить себя с толку.
+			</p>
+		</div>
+	</section>
+
+	<section id="house-types" class="house-types">
+		<div class="_container">
+			<h2 class="title">Типы домов</h2>
+
+			<div class="house-types__row d-flex">
+
+				<a href="#" class="house-card__body d-flex">
+					<div class="house-card__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/card-houses/01.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/card-houses/01.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="house-card__text">
+						<h3>Квадро</h3>
+						<p class="house-card__descp">Из оцилиндрованого бревна</p>
+					</div>
+					<div class="house-card__price-item d-flex">
+						<p class="house-card__volume btn">400 м2</p>
+						<div class="house-card__block-price">
+							<p class="house-card__price">3 000 000 р</p>
+							<p class="house-card__price-old">3 750 000 р</p>
+						</div>
+					</div>
+				</a>
+
+				<a href="#" class="house-card__body d-flex">
+					<div class="house-card__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/card-houses/02.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/card-houses/02.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="house-card__text">
+						<h3>Усадьба</h3>
+						<p class="house-card__descp">Дома из клееного бруса</p>
+					</div>
+					<div class="house-card__price-item d-flex">
+						<p class="house-card__volume btn">200 м2</p>
+						<div class="house-card__block-price">
+							<p class="house-card__price">2 500 000 р</p>
+							<p class="house-card__price-old">2 950 000 р</p>
+						</div>
+					</div>
+				</a>
+
+				<a href="#" class="house-card__body d-flex">
+					<div class="house-card__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/card-houses/03.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/card-houses/03.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="house-card__text">
+						<h3>Коттедж 1</h3>
+						<p class="house-card__descp">Бани</p>
+					</div>
+					<div class="house-card__price-item d-flex">
+						<p class="house-card__volume btn">100 м2</p>
+						<div class="house-card__block-price">
+							<p class="house-card__price">1 100 000 р</p>
+							<p class="house-card__price-old">1 450 000 р</p>
+						</div>
+					</div>
+				</a>
+
+				<a href="#" class="house-card__body d-flex">
+					<div class="house-card__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/card-houses/04.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/card-houses/04.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="house-card__text">
+						<h3>Коттедж 2</h3>
+						<p class="house-card__descp">Дома из профилированного бруса</p>
+					</div>
+					<div class="house-card__price-item d-flex">
+						<p class="house-card__volume btn">70 м2</p>
+						<div class="house-card__block-price">
+							<p class="house-card__price">900 000 р</p>
+							<p class="house-card__price-old">950 000 р</p>
+						</div>
+					</div>
+				</a>
+
+			</div>
+
+		</div>
+	</section>
+
+	<section id="gallery-project" class="gallery-project">
+		<div class="_container">
+			<h2 class="title">Фотогалерея проекта</h2>
+			<div class="gallery-project__row d-flex">
+				<div class="gallery-project__col gallery-project__col_left">
+					<div class="gallery-project__img-big">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/gallery-project/01.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/gallery-project/01.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+				</div>
+				<div class="gallery-project__col d-flex">
+					<div class="gallery-project__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/gallery-project/02.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/gallery-project/02.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="gallery-project__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/gallery-project/03.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/gallery-project/03.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="gallery-project__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/gallery-project/04.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/gallery-project/04.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+				</div>
+			</div>
+			<a href="#" class="gallery-project__btn btn">перейти в галерею</a>
+		</div>
+	</section>
+
+	<section id="follow-sec" class="follow-sec">
+		<div class="_container">
+			<div class="follow-sec__bg d-flex">
+				<div class="follow-sec__img-tel">
+					<picture>
+						<source srcset="<?php echo get_template_directory_uri(); ?>/img/instagram/01.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram/01.png?_v=1632757662937" alt="">
+					</picture>
+				</div>
+				<div class="follow-sec__title">
+					<h2>Следите за нашим проектом в Instagram</h2>
+					<a href="#" class="follow-sec__link"></a>
+				</div>
+				<div class="follow-sec__img-house">
+					<picture>
+						<source srcset="<?php echo get_template_directory_uri(); ?>/img/instagram/02.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/instagram/02.png?_v=1632757662937" alt="">
+					</picture>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="index-map" class="index-map">
+		<div class="_container">
+			<h2 class="title">Как добраться</h2>
+		</div>
+		<div id="map" class="index-map__map map"></div>
+	</section>
+
+	<section id="team" class="team">
+		<div class="_container">
+			<h2 class="title">Наша команда</h2>
+
+			<div class="team__row d-flex">
+
+				<div class="team-card__body d-flex">
+					<div class="team-card__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/team/01.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/team/01.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="team-card__text">
+						<h3>Иванова Дарья Васильевна</h3>
+						<p class="team-card__descp">Директор по развитию</p>
+					</div>
+					<div class="team-card__contact d-flex">
+						<div class="team-card__line"></div>
+						<a href="tel:+7(495)1234567" class="team-card__tel">(495) 123-45-67</a>
+						<a href="mailto:random@mail.com" class="team-card__email">random@mail.com</a>
+					</div>
+				</div>
+
+				<div class="team-card__body d-flex">
+					<div class="team-card__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/team/02.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/team/02.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="team-card__text">
+						<h3>Петров Алексей Петрович</h3>
+						<p class="team-card__descp">Исполнительный директор</p>
+					</div>
+					<div class="team-card__contact d-flex">
+						<div class="team-card__line"></div>
+						<a href="tel:+7(495)1234567" class="team-card__tel">(495) 123-45-67</a>
+						<a href="mailto:random@mail.com" class="team-card__email">random@mail.com</a>
+					</div>
+				</div>
+
+				<div class="team-card__body d-flex">
+					<div class="team-card__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/team/03.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/team/03.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="team-card__text">
+						<h3>Кисегач Алена Борисовна</h3>
+						<p class="team-card__descp">Заместитель директора</p>
+					</div>
+					<div class="team-card__contact d-flex">
+						<div class="team-card__line"></div>
+						<a href="tel:+7(495)1234567" class="team-card__tel">(495) 123-45-67</a>
+						<a href="mailto:random@mail.com" class="team-card__email">random@mail.com</a>
+					</div>
+				</div>
+
+				<div class="team-card__body d-flex">
+					<div class="team-card__img">
+						<picture>
+							<source srcset="<?php echo get_template_directory_uri(); ?>/img/team/04.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/team/04.jpg?_v=1632757662937" alt="">
+						</picture>
+					</div>
+					<div class="team-card__text">
+						<h3>Маслов Александр Иванович</h3>
+						<p class="team-card__descp">Архитектор</p>
+					</div>
+					<div class="team-card__contact d-flex">
+						<div class="team-card__line"></div>
+						<a href="tel:+7(495)1234567" class="team-card__tel">(495) 123-45-67</a>
+						<a href="mailto:random@mail.com" class="team-card__email">random@mail.com</a>
+					</div>
+				</div>
+
+			</div>
+
+			<a href="#" class="team__btn btn">Все сотрудники</a>
+
+		</div>
+	</section>
+
+	<section id="quest-work" class="quest-work">
+		<div class="_container">
+			<div class="quest-work__block-bg _bgi d-flex">
+				<div class="quest-work__img">
+					<picture>
+						<source srcset="<?php echo get_template_directory_uri(); ?>/img/vacancies.webp" type="image/webp"><img src="<?php echo get_template_directory_uri(); ?>/img/vacancies.png?_v=1632757662937" alt="">
+					</picture>
+				</div>
+				<div class="quest-work__title-block">
+					<h2>Хотите работать с нами?</h2>
+					<a href="#" class="quest-work__btn btn">Смотреть все вакансии</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="consult-sec" class="consult-sec">
+		<div class="_container">
+			<div class="consult-sec__row d-flex">
+				<div class="consult-sec__title-block d-flex">
+					<h2>Нужна <br> консультация?</h2>
+					<p>
+						Подробно расскажем о нашем коттеджном поселке, ходе строительства, проектах коттеджей и таунхаусов, ознакомим
+						с инфраструктурой и предложим самое выгодное предложение.
+					</p>
+				</div>
+				<a href="#" class="consult-sec__btn btn">Задать вопрос</a>
+			</div>
+		</div>
+	</section>
+
+</main>
+
+<?php get_footer(); ?>
